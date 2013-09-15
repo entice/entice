b9be68cc220f4148a610d625485f95c95eea0946
miracle444 <miracle444444@gmail.com>
2013-09-15 17:01:01 +0200

    added name component

---
+++ client/Protocol/Components/Name.cs
@@ -0,0 +1,43 @@
+﻿using System.Runtime.Serialization;
+using Protocol.Messages;
+using Protocol.Types;
+
+namespace Protocol.Components
+{
+        [DataContract]
+        public class Name : Component
+        {
+                [DataMember] private string name;
+                [DataMember] private string type;
+
+
+                public Name(string name)
+                {
+                        Type = GetType().Name;
+                        Value = name;
+                }
+
+                public Name(IncomingComponent incomingComponent)
+                {
+                        Type = GetType().Name;
+                        Value = incomingComponent.Name;
+                }
+
+                public string Type
+                {
+                        get { return type; }
+                        private set { type = value; }
+                }
+
+                public string Value
+                {
+                        get { return name; }
+                        private set { name = value; }
+                }
+
+                public IncomingComponent ToIncomingComponent()
+                {
+                        return new IncomingComponent {Type = Type, Name = Value};
+                }
+        }
+}
