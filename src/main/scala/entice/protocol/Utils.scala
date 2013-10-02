6b5d7ac111f6ef83b44675f83e40ef36034e9ae8
ephe-meral <j.appel.online@gmail.com>
2013-10-03 01:13:41 +0200

    added emotes enum

--- src/main/scala/entice/protocol/Utils.scala
+++ src/main/scala/entice/protocol/Utils.scala
@@ -31,18 +31,69 @@ object UUID {
  * State of a moving entity
  */
 object MoveState extends Enumeration {
-   val NotMoving    = Value("NotMoving")
-   val Moving       = Value("Moving")
+    val NotMoving    = Value("NotMoving")
+    val Moving       = Value("Moving")
 }
 
 
 /**
- * Some animations
+ * Some animations (emotes)
  */
 object Animations extends Enumeration {
-   val None         = Value("none")
-   val Dance        = Value("dance")
-   // TODO complete me pls...
+    val None                    = Value("none")
+
+    val Beg                     = Value("beg")  
+    val Doh                     = Value("doh")  
+    val Scare                   = Value("scare")    
+    val PointNod                = Value("pointNod")        
+    val Sit                     = Value("sit")  
+    val Bow                     = Value("bow")  
+    val Wave                    = Value("wave")   
+    val HighFive                = Value("highFive")        
+    val Tired                   = Value("tired")    
+    val Rock                    = Value("rock")   
+    val IdleChatting            = Value("idleChatting")            
+    val HandsOnHeadLookDown     = Value("handsOnHeadLookDown")                      
+    val Yawn                    = Value("yawn")   
+    val Shoo                    = Value("shoo")   
+    val Clap                    = Value("clap")   
+    val PickMe1                 = Value("pickMe1")       
+    val Jump                    = Value("jump")   
+    val Cheer1                  = Value("cheer1")     
+    val Pout                    = Value("pout")   
+    val Flex                    = Value("flex")   
+    val QuickFlex               = Value("quickFlex")         
+    val IdlePonder              = Value("idlePonder")          
+    val Dance                   = Value("dance")    
+    val Agree                   = Value("agree")    
+    val Flute                   = Value("flute")    
+    val Laugh                   = Value("laugh")    
+    val Cough                   = Value("cough")    
+    val Kneel                   = Value("kneel")    
+    val Cheer2                  = Value("cheer2")     
+    val Paper                   = Value("paper")    
+    val Clapping                = Value("clapping")       
+    val ClappingExcited         = Value("clappingExcited")               
+    val Drums                   = Value("drums")    
+    val Point                   = Value("point")    
+    val Taunt                   = Value("taunt")    
+    val CatchBreath1            = Value("catchBreath1")            
+    val Shoo2                   = Value("shoo2")    
+    val BowHead                 = Value("bowHead")       
+    val BothHandsUp             = Value("bothHandsUp")            
+    val PickMe2                 = Value("pickMe2")       
+    val Attention1              = Value("attention1")         
+    val Scratch                 = Value("scratch")      
+    val ShakeFist               = Value("shakeFist")         
+    val CatchBreath2            = Value("catchBreath2")            
+    val Violin                  = Value("violin")     
+    val Rank                    = Value("rank")   
+    val Guitar                  = Value("guitar")     
+    val Ponder                  = Value("ponder")     
+    val Attention2              = Value("attention2")         
+    val Scissors                = Value("scissors")       
+    val StandUp                 = Value("standUp")       
+    val Attention3              = Value("attention3")         
 }
 
 
