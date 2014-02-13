400323a9767c759a93039e163bc67d98724ae2dd
ephe-meral <j.appel.online@gmail.com>
2014-02-13 16:37:17 +0100

    added better logging for json deserialization

--- src/main/scala/entice/protocol/utils/Pipeline.scala
+++ src/main/scala/entice/protocol/utils/Pipeline.scala
@@ -20,7 +20,7 @@ object PipelineFactory {
     def getWithLog(log: LoggingAdapter) = {
         TcpPipelineHandler.withLogger(log,
 
-        new MessageStage >>
+        new MessageStage(log) >>
         new StringByteStringAdapter("utf-8") >>
         new LengthFieldFrame(
             maxSize = Int.MaxValue,
@@ -33,16 +33,28 @@ object PipelineFactory {
 /**
  * De/serializes a json object to/from a message case-class
  */
-class MessageStage extends SymmetricPipelineStage[PipelineContext, Message, String] {
+class MessageStage(val log: LoggingAdapter) extends SymmetricPipelineStage[PipelineContext, Message, String] {
  
     override def apply(ctx: PipelineContext) = new SymmetricPipePair[Message, String] {
 
         override val commandPipeline = { msg: Message =>
-            ctx.singleCommand(Json.toJson(msg).toString)
+            try {   
+                ctx.singleCommand(Json.toJson(msg).toString)
+            } catch {
+                case exc: Throwable => 
+                    log.error(exc, "Failed to serialize a json object. The object was: {}", msg)
+                    ctx.nothing
+            }
         }
  
         override val eventPipeline = { js: String =>
-            ctx.singleEvent(Json.fromJson[Message](Json.parse(js)).get)
+            try {   
+                 ctx.singleEvent(Json.fromJson[Message](Json.parse(js)).get)
+            } catch {
+                case exc: Throwable => 
+                    log.error(exc, "Failed to deserialize a json object. The string was: {}", js)
+                    ctx.nothing
+            }
         }
     }
 }
