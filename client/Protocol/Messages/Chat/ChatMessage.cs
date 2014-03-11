08084d7a26a9251fe64f7bd1dee33083c7e5533e
miracle444 <miracle444444@gmail.com>
2014-03-11 21:23:50 +0100

    update

--- client/Protocol/Messages/Chat/ChatMessage.cs
+++ client/Protocol/Messages/Chat/ChatMessage.cs
@@ -5,17 +5,19 @@ namespace Protocol.Messages.Chat
 {
         public class ChatMessage : Message
         {
-                [DataMember] public string message;
                 [DataMember] public Entity sender;
+                [DataMember] public string message;
+                [DataMember] public string channel;
 
                 public ChatMessage()
                 {
                 }
 
-                public ChatMessage(UUID sender, string message)
+                public ChatMessage(UUID sender, string message, string channel)
                 {
                         this.sender = new Entity(sender);
                         this.message = message;
+                        this.channel = channel;
                 }
         }
 }
