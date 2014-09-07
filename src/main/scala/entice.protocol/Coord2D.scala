1c51f7a9a0607426c4b4fd21ebb59dff4dd16c97
ephe-meral <j.appel.online@gmail.com>
2014-09-07 20:29:43 +0700

    server protocol implementation moved to server

--- src/main/scala/entice.protocol/Coord2D.scala
+++
@@ -1,9 +0,0 @@
-/**
- * For copyright information see the LICENSE document.
- */
-
-package entice.protocol
-
-
-/** Resembles a 2d vector or point (depending on the context) */
-case class Coord2D(x: Float, y: Float)
